a65d1825ba896d2cfc437660fffb4d14d77d1a60
TORY <tory_lu@outlook.com>
2017-12-22 12:52:11 -0500

    Update the Lambda Function

--- lambdas/AccpetInvitation/AcceptInvitation.py
+++ lambdas/AccpetInvitation/AcceptInvitation.py
@@ -2,132 +2,199 @@ import boto3
 from pyfcm import FCMNotification
 from decimal import *
 from kafka import SimpleProducer, KafkaClient
+import json
+
+import logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
 
 def lambda_handler(event, context):
 
-	# data = json.loads(json.dumps(event['queryStringParameters']))
-	# meetingId = data['MeetingId']
-	# userId = data['UserId']
-	# isAccepted = data['IsAccepted']
-	# deviceToken = data['DeviceToken']
+    logger.info("here")
+    data = event['body']
+    meetingId = data['MeetingId']
+    userId = data['UserId']
+    isAccepted = data['IsAccepted']
+    # deviceToken = data['DeviceToken']
+    push_service = FCMNotification(api_key="AAAAI7QSMgk:APA91bGiQ4Cbi9L1dnDyixObLdIOiBVBeBpwuiR8y0Am1WoUXK09UoM6uJCXo2S_gzqFycVbhd9YADyn4hYBdcNWqTfOqA2Ew_xgjz7eK5cqCOBkq3fvmCZbwOWIVWoqQkVJhsCHqkhr")
+    mycontext = Context(prec=6, rounding=ROUND_HALF_DOWN)
+    setcontext(mycontext)
+    # userId = '1567685709934136'
+    # data = {}
+    # data['latitude'] = Decimal('1.3')
+    # data['longitude'] = Decimal('1.3')
+    # data['preference'] = 'Cafe'
+    # isAccepted = True
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-1', endpoint_url="http://dynamodb.us-east-1.amazonaws.com")
+    meeting_table = dynamodb.Table('MeetingTable')
+    meeting_status_table = dynamodb.Table('MeetingStatus')
+    user_table = dynamodb.Table('Users')
 
-	mycontext = Context(prec=6, rounding=ROUND_HALF_DOWN)
-	setcontext(mycontext)
-	userId = 'Mike'
-	data = {}
-	data['latitude'] = Decimal('1.3')
-	data['longitude'] = Decimal('1.3')
-	data['preference'] = 'Cafe'
-	isAccepted = True
-	dynamodb = boto3.resource('dynamodb', region_name='us-east-1', endpoint_url="http://dynamodb.us-east-1.amazonaws.com")
-	table = dynamodb.Table('MeetingTable')
+    # sns = boto3.client('sns',aws_access_key_id= "AKIAIJSA22F7CLH7K7IQ",aws_secret_access_key="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", region_name = "us-east-1")
+    # jsonData = {
+    # 	"meetingID" : 0001,
+    # 	"meetingDate" : "12/15/2017",
+    # 	"meetingTime" : "12:00",
+    # 	"preference" : ["Indian", "Chinese"],
+    # 	"locations" : [
+    # 		{
+    # 			"location" : {
+    # 				"lat" : 40.7295174,
+    # 				"lon" : -73.9986496
+    # 			}
+    # 		},
+    # 		{
+    # 			"location" : {
+    # 				"lat" : 40.7291272,
+    # 				"lon" : -74.0019541
+    # 			}
+    # 		},
+    # 		{
+    # 			"location" : {
+    # 				"lat" : 40.7260051,
+    # 				"lon" : -74.0007954
+    # 			}
+    # 		}
+    # 	]
+    # }
+    meeting_item = meeting_table.get_item(
+         Key={
+            'MeetingId': meetingId
+        }
+    )
+    meeting_status_item = meeting_status_table.get_item(
+        Key={
+            'userID': userId
+        }
+    )
+    organizer_data = user_table.get_item(
+        Key={
+            'ID': meeting_item['Item']['organizer'],
+        }
+    )
+    organizer_token = organizer_data['Item']['Token']
 
-	response = table.get_item(
-		Key={
-	        'MeetingId': 'lala@2017-12-13 03:07:50',
-	    }
-	)
-	# if user accpet this invitation
-	if isAccepted:
-		AcceptStatus = response['Item']['acceptStatus']
-		userAcceptStatus = AcceptStatus[userId]
-		userAcceptStatus['status'] = True
-		flag = True
+    # if user accepts this invitation
+    if isAccepted:
+        print(meeting_item)
+        userName = data['userName']
+        push_service.notify_single_device(registration_id=organizer_token, 
+                                          message_body="Your invitation was accepted by "+userName, 
+                                          message_title="Meet Me Halfway")
+        user_pending_meetings = meeting_status_item['Item']['pending']
+        user_accepted_meetings = meeting_status_item['Item']['accepted']
+        user_accepted_meetings.append(meetingId)
+        if meetingId in user_pending_meetings:
+            user_pending_meetings.remove(meetingId)
+        meeting_status_table.update_item(
+            Key={
+                'userID': userId
+            },
+            UpdateExpression='set pending = :p, accepted = :a',
+            ExpressionAttributeValues={
+                ':p': user_pending_meetings,
+                ':a': user_accepted_meetings
+            }
+        )
+        AcceptStatus = meeting_item['Item']['acceptStatus']
+        AcceptStatus[userId] = [Decimal(data['latitude']), Decimal(data['longitude'])]
+        flag = True
 
-		kafka_data_message = {}
-		kafka_data_message['Locations'] = []
-		kafka_data_message['Preference'] = []
-		for key in AcceptStatus:
-			if not AcceptStatus[key]['status']:
-				flag = False
-				break
-			if 'coordinate' in AcceptStatus[key]:
-				coordinate = {
-					"latitude": AcceptStatus[key]['coordinate']['latitude'],
-					"longitude": AcceptStatus[key]['coordinate']['longitude']
-				}
-				kafka_data_message['Locations'].append(coordinate)
-				kafka_data_message['Preference'].append(AcceptStatus[key]['preference'])
-		# All the member accept the invitation
-		if flag:
-			# send message to kafka
-			kafka = KafkaClient("localhost:9092")
-			producer = SimpleProducer(client, async = True,
-                          batch_send_every_n = 1000,
-                          batch_send_every_t = 10)
-			producer.send_messages(b'Tweets', json.dumps(kafka_data_message))
-			print("lal")
-		else:
-			AcceptStatus[userId]['coordinate'] = {
-				'latitude': Decimal(data['latitude']),
-				'longitude': Decimal(data['longitude'])
-			}
-			print(AcceptStatus[userId])
-			AcceptStatus[userId]['preference'] = data['preference']
-			AcceptStatus[userId]['status'] = True
-			table.update_item(
-				Key={
-			        'MeetingId': 'lala@2017-12-13 03:07:50',
-			    },
-			    UpdateExpression='SET acceptStatus = :val',
-			    ExpressionAttributeValues={
-			        ':val': AcceptStatus
-			    }
-			)
-	else:
-		AcceptStatus = response['Item']['acceptStatus']
-		userAcceptStatus = AcceptStatus[userId]
-		if len(AcceptStatus) <= 1:
-			# notify organizer that no one accepts the invitation
-			push_service = FCMNotification(api_key="AAAAI7QSMgk:APA91bGiQ4Cbi9L1dnDyixObLdIOiBVBeBpwuiR8y0Am1WoUXK09UoM6uJCXo2S_gzqFycVbhd9YADyn4hYBdcNWqTfOqA2Ew_xgjz7eK5cqCOBkq3fvmCZbwOWIVWoqQkVJhsCHqkhr")
-			data_message = {
-					'EventTpe': 'AcceptInvitation',
-					'Response': 'All members decline the invitation'
-			}
-			result = push_service.notify_single_device(registration_id = "f-Fd1NnGd2w:APA91bEBgkS3XFxjx7kcGR-aTSpujFuogQKQP2VKFKPVueb5mp8sqoJ-850w3AHNr_3xFTqFt9mbEpd4qH4cNnok0rQyNdR38vunvevmxlmEsnZYSylluDlsN1QzeRmiwyJxOlDekuCX", message_body = "HelloWorld", data_message = data_message)
-			print("No one accepts the invitation")
-		else:
-			AcceptStatus.pop(userId, None)
-			table.update_item(
-				Key={
-			        'MeetingId': 'lala@2017-12-13 03:07:50',
-			    },
-			    UpdateExpression='SET acceptStatus = :val',
-			    ExpressionAttributeValues={
-			        ':val': AcceptStatus
-			    }
-			)
+        kafka_data_message = {}
+        kafka_data_message['Locations'] = []
+        kafka_data_message['Preference'] = meeting_item['Item']['preference']
+        kafka_data_message['DeviceToken'] = data['DeviceToken']
+        for key in AcceptStatus:
+            if  len(AcceptStatus[key]) == 0:
+                flag = False
+                break
+            else:
+                coordinate = {
+                    "latitude": AcceptStatus[key][0],
+                    "longitude": AcceptStatus[key][1]
+                }
+                kafka_data_message['Locations'].append(coordinate)
+        # All the member accept the invitation
+        if flag:
+            # send message to kafka
+            client = KafkaClient("54.84.107.121:9092")
+            producer = SimpleProducer(client, async = True,
+                                      batch_send_every_n = 1000,
+                                      batch_send_every_t = 10)
+            producer.send_messages(b'recommend_query', json.dumps(kafka_data_message))
+            # sns.publish(TopicArn = 'arn:aws:sns:us-east-1:710467018335:NotifyRecomEngine',Subject = 'Hello', MessageStructure="json", Message=json.dumps({"default":json.dumps(jsonData)}))
+        else:
+            print(AcceptStatus[userId])
+            # AcceptStatus[userId]['preference'] = data['preference']
+            meeting_table.update_item(
+                Key={
+                    'MeetingId': meetingId,
+                },
+                UpdateExpression='SET acceptStatus = :val',
+                ExpressionAttributeValues={
+                    ':val': AcceptStatus
+                }
+            )
+    else:
+        user_pending_meetings = meeting_status_item['Item']['pending']
+        if meetingId in user_pending_meetings:
+            user_pending_meetings.remove(meetingId)
+        meeting_status_table.update_item(
+            Key={
+                'userID': userId
+            },
+            UpdateExpression='set pending = :p',
+            ExpressionAttributeValues={
+                ':p': user_pending_meetings
+            }
+        )
+        AcceptStatus = meeting_item['Item']['acceptStatus']
+        if len(AcceptStatus) <= 1:
+            # notify organizer that no one accepts the invitation
+            push_service.notify_single_device(registration_id=organizer_token, message_body="All members have declined the invitation", message_title="Meet Me Halfway")
+            print("No one accepts the invitation")
+        else:
+            AcceptStatus.pop(userId, None)
+            meeting_table.update_item(
+                Key={
+                    'MeetingId': meetingId,
+                },
+                UpdateExpression='SET acceptStatus = :val',
+                ExpressionAttributeValues={
+                    ':val': AcceptStatus
+                }
+            )
 
-			# if all the members accept the invitation
-			kafka_data_message = {}
-			kafka_data_message['Locations'] = []
-			kafka_data_message['Preference'] = []
-			for key in AcceptStatus:
-				if not AcceptStatus[key]['status']:
-					flag = False
-					break
-				coordinate = {
-					"latitude": AcceptStatus[key]['coordinate']['latitude'],
-					"longitude": AcceptStatus[key]['coordinate']['longitude']
-				}
-				kafka_data_message['Locations'].append(coordinate)
-				kafka_data_message['Preferences'].append(AcceptStatus[key]['preference'])
-			# All the member accept the invitation
-			if flag:
-				# send message to kafka
-				kafka = KafkaClient("localhost:9092")
-				producer = SimpleProducer(client, async = True,
-	                          batch_send_every_n = 1000,
-	                          batch_send_every_t = 10)
-				producer.send_messages(b'Tweets', json.dumps(kafka_data_message))
+            # if all the members accept the invitation
+            kafka_data_message = {}
+            kafka_data_message['Locations'] = []
+            kafka_data_message['Preference'] = []
+            flag = True
+            for key in AcceptStatus:
+                if len(AcceptStatus[key]) == 0:
+                    flag = False
+                    break
+                coordinate = {
+                    "latitude": AcceptStatus[key][0],
+                    "longitude": AcceptStatus[key][1]
+                }
+                kafka_data_message['Locations'].append(coordinate)
+                # kafka_data_message['Preferences'].append(AcceptStatus[key]['preference'])
+            # All the member accept the invitation
+            if flag:
+                # send message to kafka
+                client = KafkaClient("54.84.107.121:9092")
+                producer = SimpleProducer(client, async = True,
+                                          batch_send_every_n = 1000,
+                                          batch_send_every_t = 10)
+                producer.send_messages(b'recommend_query', json.dumps(kafka_data_message))
 
 
 
 
-	HTTPResponse = {}
-	HTTPResponse['statusCode'] = 200
-	HTTPResponse['headers'] = {}
-	HTTPResponse['body'] = response
+# HTTPResponse = {}
+# HTTPResponse['statusCode'] = 200
+# HTTPResponse['headers'] = {}
+# HTTPResponse['body'] = response
 
-if __name__ == '__main__':
-    lambda_handler(None, None)
---
+++ lambdas/Confirmation/lambda_function.py
@@ -0,0 +1,43 @@
+import boto3
+import json
+from pyfcm import FCMNotification
+
+def lambda_handler(event, context):
+    # TODO implement
+	data = json.loads(json.dumps(event['queryStringParameters']))
+	meetingId = data['meetingId']
+	placeId = data['placeId']
+	dynamodb = boto3.resource('dynamodb', region_name='us-east-1', endpoint_url="http://dynamodb.us-east-1.amazonaws.com")
+	table = dynamodb.Table('MeetingTable')
+	
+	table.update_item(
+	        Key={
+	            'MeetingId': meetingId,
+	        },
+	        UpdateExpression='SET finalLocation = :val, Status = :val2',
+	        ExpressionAttributeValues={
+			    ':val': placeId,
+			    ':val2': true
+			}
+	    )
+	    
+	meeting = table.get_item({
+		Key={
+			'MeetingId': meetingId,
+		},
+	})
+	
+	friends = meeting['Item']['acceptStatus']
+	
+	push_service = FCMNotification(api_key="AAAAI7QSMgk:APA91bGiQ4Cbi9L1dnDyixObLdIOiBVBeBpwuiR8y0Am1WoUXK09UoM6uJCXo2S_gzqFycVbhd9YADyn4hYBdcNWqTfOqA2Ew_xgjz7eK5cqCOBkq3fvmCZbwOWIVWoqQkVJhsCHqkhr")
+	for friend in friends:
+    	table = dynamodb.Table('Users')
+		response = table.get_item(
+			Key={
+		        'ID': friend,
+		    }
+		)
+		deviceToken = response['Item']['Token']
+    	result = push_service.notify_single_device(registration_id = deviceToken, message_body = "The meeting has been confirmed")
+	    
+    return 'Hello from Lambda'
