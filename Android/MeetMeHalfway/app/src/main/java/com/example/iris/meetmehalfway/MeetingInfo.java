019723f2b23344b523af8c92375fdcdc11be3220
ptchen <irischentpe@gmail.com>
2017-12-23 21:37:46 -0500

    updatemeetinginfo-modify

--- Android/MeetMeHalfway/app/src/main/java/com/example/iris/meetmehalfway/MeetingInfo.java
+++ Android/MeetMeHalfway/app/src/main/java/com/example/iris/meetmehalfway/MeetingInfo.java
@@ -107,7 +107,7 @@ public class MeetingInfo extends AppCompatActivity{
             Log.d("asyncLoad", meetingID);
             CognitoCachingCredentialsProvider credentialsProvider = new CognitoCachingCredentialsProvider(
                     getApplicationContext(),    /* get the context for the application */
-                    "us-east-1:502f9e0a-db62-4eb0-81fe-586814b7a8d2",    /* Identity Pool ID */
+                    "*********************",    /* Identity Pool ID */
                     Regions.US_EAST_1           /* Region for your identity pool--US_EAST_1 or EU_WEST_1*/
             );
             AmazonDynamoDBClient ddbClient = new AmazonDynamoDBClient(credentialsProvider);
